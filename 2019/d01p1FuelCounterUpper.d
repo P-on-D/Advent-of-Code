9e7438fc757045d6a1a1a57ae45cd5b501223521
Paul Mitchell <coder@paul-mitchell.me.uk>
2019-12-23 18:29:08 +0000

    2019 d1 pt2 (wip) Test for updated fuel calculation

--- 2019/d01p1FuelCounterUpper.d
+++ 2019/d01p1FuelCounterUpper.d
@@ -4,6 +4,15 @@ unittest {
   auto input = [12, 14, 1969, 100756];
   auto fuelTotal = input.map!"a / 3 - 2".sum;
   assert(fuelTotal == 2 + 2 + 654 + 33583); // 34241
+
+  int fuelFor(int mass) {
+    int fuel = mass / 3 - 2;
+    return fuel <= 0 ? 0 : fuel + fuelFor(fuel);
+  }
+
+  assert(fuelFor(14) == 2);
+  assert(fuelFor(1969) == 966);
+  assert(fuelFor(100756) == 50346);
 } version(unittest) {} else {
 
 void main() {
