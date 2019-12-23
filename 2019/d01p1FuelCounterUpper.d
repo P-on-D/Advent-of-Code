082af37e23de537c048ee30353dfd58ad1bc93ff
Paul Mitchell <coder@paul-mitchell.me.uk>
2019-12-23 18:31:31 +0000

    2019 d1 pt2 ✔️
    4687331

--- 2019/d01p1FuelCounterUpper.d
+++ 2019/d01p1FuelCounterUpper.d
@@ -1,14 +1,14 @@
 import std.algorithm;
 
+int fuelFor(int mass) {
+  int fuel = mass / 3 - 2;
+  return fuel <= 0 ? 0 : fuel + fuelFor(fuel);
+}
+
 unittest {
   auto input = [12, 14, 1969, 100756];
   auto fuelTotal = input.map!"a / 3 - 2".sum;
   assert(fuelTotal == 2 + 2 + 654 + 33583); // 34241
-
-  int fuelFor(int mass) {
-    int fuel = mass / 3 - 2;
-    return fuel <= 0 ? 0 : fuel + fuelFor(fuel);
-  }
 
   assert(fuelFor(14) == 2);
   assert(fuelFor(1969) == 966);
@@ -17,7 +17,7 @@ unittest {
 
 void main() {
   import std.conv, std.stdio;
-  stdin.byLineCopy.map!(to!int).map!"a / 3 - 2".sum.writeln;
+  stdin.byLineCopy.map!(to!int).map!fuelFor.sum.writeln;
 }
 
 }
