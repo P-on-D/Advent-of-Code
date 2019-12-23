bb61f5f127be637f1e338a0922c0788506f66359
Paul Mitchell <coder@paul-mitchell.me.uk>
2019-12-23 18:10:48 +0000

    2019 d1 pt1 (wip) Example input from stdin
    $ docker run --rm -v$PWD:/src d sh -c 'rdmd d01p1FuelCounterUpper.d < d01p1FuelCounterUpper.txt'

--- 2019/d01p1FuelCounterUpper.d
+++ 2019/d01p1FuelCounterUpper.d
@@ -1,7 +1,14 @@
-unittest {
-  import std.algorithm;
+import std.algorithm;
 
+unittest {
   auto input = [12, 14, 1969, 100756];
   auto fuelTotal = input.map!"a / 3 - 2".sum;
-  assert(fuelTotal == 2 + 2 + 654 + 33583);
+  assert(fuelTotal == 2 + 2 + 654 + 33583); // 34241
+} version(unittest) {} else {
+
+void main() {
+  import std.conv, std.stdio;
+  stdin.byLineCopy.map!(to!int).map!"a / 3 - 2".sum.writeln;
+}
+
 }
