fcf7a0a97a2aa0dd7e507ad42052586da7b2c8db
Paul Mitchell <coder@paul-mitchell.me.uk>
2019-12-24 19:54:35 +0000

    OK, that's a good start

--- 2019/d03p1LineCrossover.d
+++ 2019/d03p1LineCrossover.d
@@ -13,6 +13,13 @@ unittest {
     return Leg(to!Dir(spec[0]), to!int(spec[1..$]));
   }
 
+  assert(toLeg("R8") == Leg(Dir.Right, 8));
+  assert(toLeg("U5") == Leg(Dir.Up, 5));
+  assert(toLeg("L5") == Leg(Dir.Left, 5));
+  assert(toLeg("D3") == Leg(Dir.Down, 3));
+  assert(toLeg("R9999") == Leg(Dir.Right, 9999));
+
+/*
   auto parseWire(T)(T spec) {
     import std.array;
 
@@ -70,4 +77,4 @@ unittest {
     "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
   , "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
   ]) == 135);
-}
+*/}
