44b6dbd701ea64f539c406c0275d232e445f8348
Paul Mitchell <coder@paul-mitchell.me.uk>
2019-12-25 23:49:15 +0000

    2019 d7 pt1 (wip) More TDD from examples

--- 2019/d07Amplifiers.d
+++ 2019/d07Amplifiers.d
@@ -10,7 +10,7 @@ auto thrusterSignal(int[] program, int[] phases) {
 }
 
 unittest {
-  import std.algorithm, std.range;
+  import std.algorithm, std.range, std.typecons;
 
   assert(iota(0, 5).permutations.count == 5 * 4 * 3 * 2);
 
@@ -31,5 +31,18 @@ unittest {
       [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0]
     , [1,0,4,3,2]
     ) == 65210
+  );
+
+  assert(
+    maxThrusterSignal([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])
+      == tuple(43210, [4,3,2,1,0])
+  );
+  assert(
+    maxThrusterSignal([3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0])
+      == tuple(54321, [0,1,2,3,4])
+  );
+  assert(
+    maxThrusterSignal([3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0])
+      == tuple(65210, [1,0,4,3,2])
   );
 }
