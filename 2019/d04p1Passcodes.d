fa8af238ff0f1d6cb55c7cedc01ebd02c151ea67
Paul Mitchell <coder@paul-mitchell.me.uk>
2019-12-25 14:39:31 +0000

    2019 d4 pt2 (wip) Tests pass for example input

--- 2019/d04p1Passcodes.d
+++ 2019/d04p1Passcodes.d
@@ -15,11 +15,38 @@ bool meetsCriteria(int input) {
   return !decreasing && doubleadjacent;
 }
 
+bool meetsNewCriteria(int input) {
+  int i = input % 10;
+  int n = input / 10;
+  int l = 1;
+  bool decreasing;
+  bool doubleobserved;
+
+  do {
+    int r = n % 10;
+    n /= 10;
+    if (r > i) { decreasing = true; break; }
+    if (r == i) {
+      ++l;
+    } else {
+      if (l==2) doubleobserved = true;
+      l = 1;
+    }
+    i = r;
+  } while(n > 0);
+
+  return !decreasing && (doubleobserved || l == 2);
+}
+
 unittest {
   assert(meetsCriteria(111111));
   assert(!meetsCriteria(223450));
   assert(!meetsCriteria(123789));
-}
+
+  assert(meetsNewCriteria(112233));
+  assert(!meetsNewCriteria(123444));
+  assert(meetsCriteria(111122));
+} version (unittest) {} else {
 
 void main() {
   int meets;
@@ -30,4 +57,6 @@ void main() {
 
   import std.stdio;
   meets.writeln;
+}
+
 }
