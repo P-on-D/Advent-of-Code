77e68c1b8e4f2b1a899e346ccbddd78382ddee18
Paul Mitchell <coder@paul-mitchell.me.uk>
2019-12-26 22:28:27 +0000

    2019 d2 pt2 ✔️
    [3100786347]
    [87023]

--- 2019/d09Boost.d
+++ 2019/d09Boost.d
@@ -3,5 +3,7 @@ import intcode;
 void main() {
   import std.algorithm, std.array, std.conv, std.stdio;
 
-  LongCode(stdin.readln.split(',').map!(to!long).array).run([1]).writeln;
+  auto BOOST = stdin.readln.split(',').map!(to!long).array;
+  LongCode(BOOST.dup).run([1]).writeln;
+  LongCode(BOOST.dup).run([2]).writeln;
 }
