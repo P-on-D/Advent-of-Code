c93ca5220fa3f8573214f16912ea012657e92500
Paul Mitchell <coder@paul-mitchell.me.uk>
2019-12-30 20:11:27 +0000

    2019 d12 pt1 (wip) More example TDD, motion simulation looks sound

--- 2019/d12Moons.d
+++ 2019/d12Moons.d
@@ -13,18 +13,67 @@ Moon toMoon(T)(T input) {
   return Moon(Pt(x, y, z));
 }
 
-unittest{
-  import std.algorithm;
+auto simulateMotion(T)(T moons) {
+  foreach(ref u; moons) {
+    foreach(ref v; moons) {
+      import std.math;
 
-  assert([
+      u.vel.dx += sgn(v.pos.x - u.pos.x);
+      u.vel.dy += sgn(v.pos.y - u.pos.y);
+      u.vel.dz += sgn(v.pos.z - u.pos.z);
+    }
+  }
+
+  foreach(ref u; moons) {
+    u.pos.x += u.vel.dx;
+    u.pos.y += u.vel.dy;
+    u.pos.z += u.vel.dz;
+  }
+
+  return moons;
+}
+
+unittest{
+  import std.algorithm, std.array;
+
+  auto moons = [
     "<x=-1, y=0, z=2>"
   , "<x=2, y=-10, z=-7>"
   , "<x=4, y=-8, z=8>"
   , "<x=3, y=5, z=-1>"
-  ].map!toMoon.equal([
+  ].map!toMoon.array;
+
+  assert(moons.equal([
     Moon(Pt(-1,0,2))
   , Moon(Pt(2,-10,-7))
   , Moon(Pt(4,-8,8))
   , Moon(Pt(3,5,-1))
+  ]));
+
+  auto simulation = moons.simulateMotion;
+
+  assert(simulation.equal([
+    Moon(Pt(2,-1,1), Vec(3,-1,-1))
+  , Moon(Pt(3,-7,-4), Vec(1,3,3))
+  , Moon(Pt(1,-7,5), Vec(-3,1,-3))
+  , Moon(Pt(2,2,0), Vec(-1,-3,1))
+  ]));
+
+  simulation.simulateMotion;
+
+  assert(simulation.equal([
+    Moon(Pt( 5, -3, -1), Vec( 3, -2, -2))
+  , Moon(Pt( 1, -2,  2), Vec(-2,  5,  6))
+  , Moon(Pt( 1, -4, -1), Vec( 0,  3, -6))
+  , Moon(Pt( 1, -4,  2), Vec(-1, -6,  2))
+  ]));
+
+  foreach(i; 2..10) simulation.simulateMotion;
+
+  assert(simulation.equal([
+    Moon(Pt( 2,  1, -3), Vec(-3, -2,  1))
+  , Moon(Pt( 1, -8,  0), Vec(-1,  1,  3))
+  , Moon(Pt( 3, -6,  1), Vec( 3,  2, -3))
+  , Moon(Pt( 2,  0,  4), Vec( 1, -1, -1))
   ]));
 }
