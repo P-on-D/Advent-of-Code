7cc7d41045df8e471e9eeb74c84ce5a7098b8e32
Paul Mitchell <coder@paul-mitchell.me.uk>
2019-12-31 13:59:32 +0000

    2019 d14 pt1 (wip) TDD. A less ambition start

--- 2019/d14OreFuel.d
+++ 2019/d14OreFuel.d
@@ -1,6 +1,36 @@
 import std.typecons;
 
+struct Nanofactory {
+  auto solveFor(N, S)(N units, S chemical) {
+    return tuple(0, "ANYTHING");
+  }
+}
+
+auto withReactions(R)(R reactions) {
+  return Nanofactory();
+}
+
 unittest {
+  assert(withReactions([
+    "1 ORE => 1 FUEL"
+  ]).solveFor(1, "ORE") == tuple(1, "ORE"));
+
+  assert(withReactions([
+    "1 ORE => 1 FUEL"
+  ]).solveFor(10, "ORE") == tuple(10, "ORE"));
+
+  assert(withReactions([
+    "1 ORE => 1 FUEL"
+  ]).solveFor(1, "FUEL") == tuple(1, "ORE"));
+
+  assert(withReactions([
+    "1 ORE => 1 FUEL"
+  ]).solveFor(10, "FUEL") == tuple(10, "ORE"));
+
+  assert(withReactions([
+    "3 ORE => 2 FUEL"
+  ]).solveFor(5, "FUEL") == tuple(9, "ORE"));
+
   assert(withReactions([
     "10 ORE => 10 A"
   , "1 ORE => 1 B"
