83677371a7a9438cccd72b74dc08501c5a76354d
Paul Mitchell <coder@paul-mitchell.me.uk>
2019-12-31 13:26:01 +0000

    2019 d13 pt2 ✔️ Animated with ANSI :)
    11140
    #########################################
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #       @                               #
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #                                       #
    #       _                               #
    #                                       #
    #  ===   =     ==  = =   ==   =   =     #
    real    0m12.085s   =  =    =  = = = =  #
    user  ==0m4.430s=  ==   ==  =   ===== = #
    sys  = =0m1.750s =  = === ===     =  =  #

--- 2019/d13Breakout.d
+++ 2019/d13Breakout.d
@@ -30,7 +30,9 @@ auto plot(T)(T[Pt] grid, char[T] lookup) {
 void main() {
   import intcode, std.array, std.conv, std.stdio;
 
-  auto breakout = LongCode(stdin.readln.split(',').map!(to!long).array);
+  auto breakoutProgram = stdin.readln.split(',').map!(to!long).array;
+
+  auto breakout = LongCode(breakoutProgram);
 
   auto x = breakout.runInFeedback;
 
@@ -42,6 +44,40 @@ void main() {
     x = breakout.cont;
   }
 
-  grid.plot([0: ' ', 1: '#', 2: '=', 3: '_', 4: '@']).each!writeln;
+  char[ubyte] symbols = [0: ' ', 1: '#', 2: '=', 3: '_', 4: '@'];
+
+  "\x1B[2J".writeln;
+
+  grid.plot(symbols).each!writeln;
   grid.values.count!"a == 2".writeln;
+
+  breakoutProgram[0] = 2;
+  breakout = LongCode(breakoutProgram);
+  auto input = 0;
+  long score = 0;
+  Pt ball, paddle;
+  grid.clear;
+
+  x = breakout.runInFeedback([input]);
+  while(!breakout.halted) {
+    auto y = breakout.cont;
+    auto t = breakout.cont;
+    if (x[0] == -1 && y[0] == 0) {
+      score = t[0];
+    } else {
+      Pt pt = Pt(cast(int)x[0], cast(int)y[0]);
+      if (t[0] == 4) ball = pt; else if(t[0] == 3) paddle = pt;
+      grid[pt] = cast(ubyte)t[0];
+    }
+
+    writeln("\x1B[3;0H", score);
+    grid.plot(symbols).each!writeln;
+
+    import std.math;
+    input = sgn(ball.x - paddle.x);
+    x = breakout.cont([input]);
+  }
+
+  writeln("\x1B[3;0H", score);
+  grid.plot(symbols).each!writeln;
 }
