25fcf09d0433ae0b857aa63b2610fd6268dab209
Paul Mitchell <coder@paul-mitchell.me.uk>
2020-01-01 13:42:45 +0000

    2019 d14 pt1 (wip) Decent start, I don't like the parser though

--- 2019/d14OreFuel.d
+++ 2019/d14OreFuel.d
@@ -1,35 +1,80 @@
-import std.typecons;
+import std.algorithm, std.array, std.range, std.typecons;
+
+alias Units = uint;
+alias Symbol = string;
+
+struct Quantity {
+  Units units;
+  Symbol of;
+}
+
+struct Reaction {
+  Quantity qty;
+  Quantity[] requires;
+}
 
 struct Nanofactory {
-  auto solveFor(N, S)(N units, S chemical) {
-    return tuple(0, "ANYTHING");
+  Reaction[] reactions;
+
+  auto solveFor(Quantity required) {
+    auto sources = reactions.find!(r => r.qty.of == required.of);
+    if (!sources.empty) {
+      return sources[0].requires[0];
+    }
+    return required;
   }
 }
 
 auto withReactions(R)(R reactions) {
-  return Nanofactory();
+  import std.conv, std.regex;
+
+  // match order is;
+  //   0: all
+  //   1: 1st requirement, 2: unit, 3: symbol
+  //   4-6: 2nd requirement if present, and so on
+  // $-3: output, $-2: unit, $-1: symbol
+  auto rxReaction = regex(r"( ?(\d+) (\w+),?)+ => ((\d+) (\w+))");
+
+  Quantity toQty(R)(R matches) {
+    return Quantity(
+      matches.dropOne.front.to!Units  // matches[1]
+    , matches.dropOne.front.to!Symbol // matches[2]
+    );
+  }
+
+  auto parser(string reactionSpec) {
+    auto matches = reactionSpec.matchFirst(rxReaction);
+    if (matches[0]) {
+      matches.popFront;
+      auto quantities = matches.chunks(3).map!(ch => toQty(ch)).array;
+      return Reaction(quantities.back, quantities.dropBackOne);
+    }
+    return Reaction();
+  }
+
+  return Nanofactory(reactions.map!(parser).array);
 }
 
 unittest {
   assert(withReactions([
     "1 ORE => 1 FUEL"
-  ]).solveFor(1, "ORE") == tuple(1, "ORE"));
+  ]).solveFor(Quantity(1, "ORE")) == Quantity(1, "ORE"));
 
   assert(withReactions([
     "1 ORE => 1 FUEL"
-  ]).solveFor(10, "ORE") == tuple(10, "ORE"));
+  ]).solveFor(Quantity(10, "ORE")) == Quantity(10, "ORE"));
 
   assert(withReactions([
     "1 ORE => 1 FUEL"
-  ]).solveFor(1, "FUEL") == tuple(1, "ORE"));
+  ]).solveFor(Quantity(1, "FUEL")) == Quantity(1, "ORE"));
 
   assert(withReactions([
     "1 ORE => 1 FUEL"
-  ]).solveFor(10, "FUEL") == tuple(10, "ORE"));
+  ]).solveFor(Quantity(10, "FUEL")) == Quantity(10, "ORE"));
 
   assert(withReactions([
     "3 ORE => 2 FUEL"
-  ]).solveFor(5, "FUEL") == tuple(9, "ORE"));
+  ]).solveFor(Quantity(5, "FUEL")) == Quantity(9, "ORE"));
 
   assert(withReactions([
     "10 ORE => 10 A"
@@ -38,5 +83,5 @@ unittest {
   , "7 A, 1 C => 1 D"
   , "7 A, 1 D => 1 E"
   , "7 A, 1 E => 1 FUEL"
-  ]).solveFor(1, "FUEL") == tuple(31, "ORE"));
+  ]).solveFor(Quantity(1, "FUEL")) == Quantity(31, "ORE"));
 }
