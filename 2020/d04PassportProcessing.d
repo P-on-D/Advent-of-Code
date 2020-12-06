acb42f679984bf1cebff1f6671cb626a7512cdaf
Paul Mitchell <coder@paul-mitchell.me.uk>
2020-12-06 18:10:08 +0000

    Day 4 "Passport Processing" Part 1 - Passport Validity TDD

--- 2020/d04PassportProcessing.d
+++ 2020/d04PassportProcessing.d
@@ -1,16 +1,28 @@
 // Advent of Code 2020 https://adventofcode.com/2020/day/4
 // Day 4: Passport Processing
 
+enum requiredFields = ["ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"];
+
 auto parsePassportBatch(R)(R input) {
-  import std.algorithm : joiner, map, splitter;
+  import std.algorithm : map, splitter;
+  import std.array : join;
 
   return input
     .splitter("")
-    .map!(chunk => chunk.joiner(" "));
+    .map!(chunk => chunk.join(" "));
+}
+
+auto passportValid(string line) {
+  import std.algorithm : all, canFind, map, splitter;
+  import std.range : front;
+
+  auto keys = line.splitter(' ').map!(pair => pair.splitter(':').front);
+
+  return requiredFields.all!(k => keys.canFind(k));
 }
 
 unittest {
-  import std.algorithm : count;
+  import std.algorithm : count, equal, map;
 
   auto input = [
     "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
@@ -28,7 +40,11 @@ unittest {
     "iyr:2011 ecl:brn hgt:59in"
   ];
 
-  assert(input.parsePassportBatch.count == 4);
+  auto passportLines = input.parsePassportBatch;
+  assert(passportLines.count == 4);
+
+  auto validity = passportLines.map!passportValid;
+  assert(validity.equal([true, false, true, false]));
 }
 
 void main() {}
