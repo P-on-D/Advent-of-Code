12d17c28511583b5f294bdfff07a8ce060491f35
Paul Mitchell <coder@paul-mitchell.me.uk>
2020-12-06 16:43:47 +0000

    Day 3 "Toboggan Trajectory" Part 2 ✔️
    9406609920

--- 2020/d03TobogganTrajectory.d
+++ 2020/d03TobogganTrajectory.d
@@ -1,22 +1,38 @@
 // Advent of Code 2020 https://adventofcode.com/2020/day/3
 // Day 3: Toboggan Trajectory
 
-auto traverseTerrain(string[] terrainMap, uint right, uint down) {
+struct Vector { uint right, down; }
+
+auto traverseTerrain(string[] terrainMap, Vector vec) {
   uint x, y;
   string encounters;
 
   while(y < terrainMap.length) {
     encounters ~= terrainMap[y][x];
 
-    x = (x + right) % terrainMap[y].length;
-    y = y + down;
+    x = (x + vec.right) % terrainMap[y].length;
+    y = y + vec.down;
   }
 
   return encounters;
 }
 
-unittest {
+auto trees(R)(R encounters) {
   import std.algorithm : count;
+
+  return encounters.count('#');
+}
+
+enum slopes = [
+  Vector(1, 1),
+  Vector(3, 1),
+  Vector(5, 1),
+  Vector(7, 1),
+  Vector(1, 2)
+];
+
+unittest {
+  import std.algorithm : equal, map;
 
   auto input = [
     "..##.......",
@@ -32,16 +48,25 @@ unittest {
     ".#..#...#.#"
   ];
 
-  assert(input.traverseTerrain(3, 1).count('#') == 7);
+  assert(input.traverseTerrain(Vector(3, 1)).trees == 7);
+  assert(
+    slopes
+      .map!(vector => input.traverseTerrain(vector).trees)
+      .equal([2, 7, 3, 4, 2])
+  );
 }
 
 void main() {
-  import std.algorithm : count, filter, map, splitter;
+  import std.algorithm : filter, fold, map, splitter;
   import std.array : array;
   import std.path : setExtension;
   import std.stdio;
 
   auto data = import(__FILE__.setExtension("txt")).splitter("\n");
   auto input = data.array;
-  input.traverseTerrain(3, 1).count('#').writeln;
+  input.traverseTerrain(Vector(3, 1)).trees.writeln;
+  slopes
+    .map!(vector => input.traverseTerrain(vector).trees)
+    .fold!((a, b) => a * b)(1L)
+    .writeln;
 }
