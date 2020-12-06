796dcd2aa859fee4ddce2952ade7f50a66fcc2b5
Paul Mitchell <coder@paul-mitchell.me.uk>
2020-12-06 14:41:00 +0000

    Day 1 "Report Repair" Part 2 ✔️
    878724
    201251610

--- 2020/d01ReportRepair.d
+++ 2020/d01ReportRepair.d
@@ -13,6 +13,19 @@ auto elementsThatSumTo(R, T)(R input, T sumTarget) {
     .front;
 }
 
+auto threeElementsThatSumTo(R, T)(R input, T sumTarget) {
+  import std.algorithm : cartesianProduct, filter, map;
+  import std.range : enumerate;
+  import std.typecons : tuple;
+
+  auto indexedInput = input.enumerate;
+  return indexedInput.cartesianProduct(indexedInput, indexedInput)
+    .filter!(a => a[0].index != a[1].index && a[0].index != a[2].index && a[1].index != a[2].index)
+    .filter!(a => a[0].value + a[1].value + a[2].value == sumTarget)
+    .map!(a => tuple(a[0].value, a[1].value, a[2].value))
+    .front;
+}
+
 unittest {
   auto input = [
     1721,
@@ -23,14 +36,23 @@ unittest {
     1456
   ];
 
-  auto result = input.elementsThatSumTo(2020);
-  assert(result[0] == 1721);
-  assert(result[1] == 299);
+  {
+    auto result = input.elementsThatSumTo(2020);
+    assert(result[0] == 1721);
+    assert(result[1] == 299);
 
-  // Defend against naïve implementation
-  result = [1010, 1721, 299].elementsThatSumTo(2020);
-  assert(result[0] == 1721);
-  assert(result[1] == 299);
+    // Defend against naïve implementation
+    result = [1010, 1721, 299].elementsThatSumTo(2020);
+    assert(result[0] == 1721);
+    assert(result[1] == 299);
+  }
+
+  {
+    auto result = input.threeElementsThatSumTo(2020);
+    assert(result[0] == 979);
+    assert(result[1] == 366);
+    assert(result[2] == 675);
+  }
 }
 
 void main() {
@@ -43,4 +65,7 @@ void main() {
   auto input = data.map!(to!int);
   auto result = input.elementsThatSumTo(2020);
   writeln(result[0] * result[1]);
+
+  auto result3 = input.threeElementsThatSumTo(2020);
+  writeln(result3[0] * result3[1] * result3[2]);
 }
