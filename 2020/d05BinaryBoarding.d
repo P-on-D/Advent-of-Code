2ebb1cf37c9027a2a9acc92cedeb453a5ed2f6e7
Paul Mitchell <coder@paul-mitchell.me.uk>
2020-12-11 19:25:00 +0000

    Day 5 "Binary Boarding" Part 2 - TDD (inefficient)

--- 2020/d05BinaryBoarding.d
+++ 2020/d05BinaryBoarding.d
@@ -10,11 +10,23 @@ auto seatID(R)(R input) {
     .to!int(2);
 }
 
+auto missingID(R)(R input) {
+  import std.algorithm : minElement, maxElement, find, canFind;
+  import std.range : front, iota;
+
+  return iota(input.minElement, input.maxElement)
+    .find!(id => !input.canFind(id))
+    .front;
+}
+
 unittest {
   assert("FBFBBFFRLR".seatID == 357);
   assert("BFFFBBFRRR".seatID == 567);
   assert("FFFBBBFRRR".seatID == 119);
   assert("BBFFBBFRLL".seatID == 820);
+
+  assert([1,2,3,5,6].missingID == 4);
+  assert([10,8,7].missingID == 9);
 }
 
 void main() {
