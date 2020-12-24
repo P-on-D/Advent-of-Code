b931f00801c037f2a999864c5c2401f2db8b3acb
Paul Mitchell <coder@paul-mitchell.me.uk>
2020-12-24 15:20:20 +0000

    Day 13 "Shuttle Search" Part 1 - TDD

---
+++ 2020/d13ShuttleSearch.d
@@ -0,0 +1,34 @@
+// Advent of Code 2020 https://adventofcode.com/2020/day/13
+// Day 13: Shuttle Search
+
+auto earliestBusAndWaitTime(string[] input) in (input.length >= 2) {
+  import std.algorithm : filter, map, minElement, splitter;
+  import std.conv : to;
+  import std.math : sgn;
+
+  auto earliestDepart = to!uint(input[0]);
+  auto busIDs = input[1].splitter(",").filter!(id => id != "x").map!(to!uint);
+
+  uint[2] firstBus(uint id) {
+    uint r = sgn(earliestDepart % id);
+    uint d = earliestDepart / id;
+    uint nextBus = id * (d + r);
+    uint wait = nextBus - earliestDepart;
+    return [id, wait];
+  }
+
+  return busIDs
+    .map!firstBus
+    .minElement!"a[1]";
+}
+
+unittest {
+  auto input = [
+    "939",
+    "7,13,x,x,59,x,31,19",
+  ];
+
+  assert(input.earliestBusAndWaitTime == [59, 5]);
+}
+
+void main() {}
